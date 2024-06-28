0cd32ee92df7ea96610ee517d7797eee498ea975
except <except134@gmail.com>
2024-06-28 18:35:45 +0300

    Added Lesson 11

---
+++ Lesson11/Lesson11.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+void print()
+{
+    std::cout << "Hello World!\n";
+}
+
+int main()
+{
+    int x = 100;
+    3 + 7;
+    int y = x + 100;
+
+    int b = 0;
+    b = b + 2;
+
+    int test;
+    test = 100500;
+
+    int test2 = 1005001;
+
+    int mult = x * y;
+    int random = 1002;
+
+    std::cout << random;
+}
+
