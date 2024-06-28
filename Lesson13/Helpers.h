722ff4784da2d07cdb6d1688544d4a676d43b86e
except <except134@gmail.com>
2024-06-28 18:56:40 +0300

    Added Lesson 13

---
+++ Lesson13/Helpers.h
@@ -0,0 +1,7 @@
+#pragma once
+
+inline int SumSqr(int v1, int v2)
+{
+    return (v1 + v2) * (v1 + v2);
+}
+
---
+++ Lesson13/Lesson13.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Helpers.h"
+
+int main()
+{
+    std::cout << SumSqr(6, 7) << "\n";
+}
+
