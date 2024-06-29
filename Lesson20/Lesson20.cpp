7b8ed27560c923e20e73aaa443145d4b5eedd027
except <except134@gmail.com>
2024-06-29 14:24:44 +0300

    Lesson 20 Structs Step 2

--- Lesson20/Lesson20.cpp
+++ Lesson20/Lesson20.cpp
@@ -5,10 +5,16 @@ struct Student
     int Age;
     int Height;
     std::string Name;
+
+    void GetInfo()
+    {
+        std::cout << "student struct";
+    }
 };
 
 int main()
 {
     Student* ptr = new Student{33, 180, "Ivan"};
+    ptr->GetInfo();
 }
 
